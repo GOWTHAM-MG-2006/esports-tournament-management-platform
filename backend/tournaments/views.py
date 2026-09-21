ba69d281f6a5133515d57954899db8cb89b1779a
GOWTHAM-MG-2006 <mggowtham2006@gmail.com>
2026-09-21 15:04:01 +0530

    fix: import sorting in tournaments/views.py

--- backend/tournaments/views.py
+++ backend/tournaments/views.py
@@ -1,6 +1,6 @@
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.exceptions import ValidationError, PermissionDenied
+from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from users.permissions import IsOrganizer
